f9e62215e7755263f0d50c7eb5921db854b9e144
DannyWang1922 <1922174653@qq.com>
2025-04-15 16:58:22 +0800

    modify run_eval.py

--- run_eval.py
+++ run_eval.py
@@ -1,32 +1,55 @@
 import subprocess
 import sys
 
-model_name_or_path  = "models/beg_defult_para"
-pooling_strategy = "cls"
-is_llm = "0"
+# model_name_or_path  = "models/beg_defult_para"
+model_name_or_path  = "models/uae_default_para"
+# model_name_or_path  = "models/qwen_default_para"
+
+nv_cmd = "NCCL_P2P_DISABLE=1 NCCL_IB_DISABLE=1 CUDA_VISIBLE_DEVICES=0"
 
 model_name = model_name_or_path.split("/")[-1]
 best_model = model_name_or_path + "/best"
 last_model = model_name_or_path + "/last"
 
 # BAAI/bge-base-en-v1.5", "WhereIsAI/UAE-Large-V1", "Qwen/Qwen1.5-0.5B"
-ese_sts_model_list = best_model+","+last_model+","+"BAAI/bge-base-en-v1.5"
+if "bge" in model_name.lower():
+    baseline_model = "BAAI/bge-base-en-v1.5"
+elif "uae" in model_name.lower():
+    baseline_model = "WhereIsAI/UAE-Large-V1"
+elif "qwen" in model_name.lower():
+    baseline_model = "Qwen/Qwen1.5-0.5B"
+else:
+    raise ValueError(f"Unknown model name: {model_name}")
 
-main_out_dir = "evl_res/main_" + model_name +"_"
-plot_out_dir = "evl_res/plot"
+ese_sts_model_list = best_model+","+last_model+","+baseline_model
 
+main_out_dir = "evl_res/"+ model_name+ "/main_" + model_name +"_"
+plot_out_dir = "evl_res/"+ model_name+"/plot"
 
-cmd_list = [
-    f"python eval_nli_main.py --is_llm {is_llm} --pooling_strategy {pooling_strategy}  --model_name_or_path {best_model} --out_dir {main_out_dir + best_model.split('/')[-1]}",
-    f"python eval_nli_main.py --is_llm {is_llm} --pooling_strategy {pooling_strategy}  --model_name_or_path {last_model} --out_dir {main_out_dir + last_model.split('/')[-1]}",
-    f"python eval_ese_sts_bench.py --is_llm {is_llm} --pooling_strategy {pooling_strategy}  --model_name_or_path_list {ese_sts_model_list} --out_dir {plot_out_dir}"
-]
+if "qwen" in model_name_or_path.lower():
+    is_llm = "1"
+    pooling_strategy = "mean"
+    prompt_template = "Represent following sentence for general embedding: {text} <|end_of_text|>"
+    cmd_list = [
+        f"python eval_nli_main.py --is_llm {is_llm} --pooling_strategy {pooling_strategy} --prompt_template {prompt_template} --model_name_or_path {best_model} --out_dir {main_out_dir + best_model.split('/')[-1]}",
+        f"python eval_nli_main.py --is_llm {is_llm} --pooling_strategy {pooling_strategy} --prompt_template {prompt_template} --model_name_or_path {last_model} --out_dir {main_out_dir + last_model.split('/')[-1]}",
+        f"python eval_ese_sts_bench.py --is_llm {is_llm} --pooling_strategy {pooling_strategy}  --prompt_template {prompt_template} --model_name_or_path_list {ese_sts_model_list} --out_dir {plot_out_dir}"
+    ]
+else:
+    is_llm = "0"
+    pooling_strategy = "cls"
+    cmd_list = [
+        f"python eval_nli_main.py --is_llm {is_llm} --pooling_strategy {pooling_strategy} --model_name_or_path {best_model} --out_dir {main_out_dir + best_model.split('/')[-1]}",
+        f"python eval_nli_main.py --is_llm {is_llm} --pooling_strategy {pooling_strategy} --model_name_or_path {last_model} --out_dir {main_out_dir + last_model.split('/')[-1]}",
+        f"python eval_ese_sts_bench.py --is_llm {is_llm} --pooling_strategy {pooling_strategy} --model_name_or_path_list {ese_sts_model_list} --out_dir {plot_out_dir}"
+    ]
 
 for cmd in cmd_list:
-    print(f"\nRunning cmd: {cmd}")
+    cmd = nv_cmd + " " + cmd
+    print(f"\nRunning cmd: {cmd}\n")
     try:
-        # subprocess.run(cmd, shell=True, check=True, stdout=sys.stdout, stderr=sys.stderr)
-        print(f"Command Done: {cmd}\n")
+        subprocess.run(cmd, shell=True, check=True, stdout=sys.stdout, stderr=sys.stderr)
+        print(f"Command Done: {cmd}")
         print("=" * 100)
     except subprocess.CalledProcessError as e:
         print(f"Command execution failure: {e}")
