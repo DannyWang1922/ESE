fba86adcc6c39dba313ef62a85274f64caa04345
DannyWang1922 <1922174653@qq.com>
2025-05-29 21:23:04 +0800

    add error log in run_train.pt

--- run_train.py
+++ run_train.py
@@ -2,19 +2,24 @@ import subprocess
 import sys
 
 cmd_list = [
-    "NCCL_P2P_DISABLE=1 NCCL_IB_DISABLE=1 CUDA_VISIBLE_DEVICES=0 python train_moe.py --config config/bert_moe_base_all.yaml",
     "NCCL_P2P_DISABLE=1 NCCL_IB_DISABLE=1 CUDA_VISIBLE_DEVICES=0 python train_moe.py --config config/bert_moe_ese_all.yaml",
+    "NCCL_P2P_DISABLE=1 NCCL_IB_DISABLE=1 CUDA_VISIBLE_DEVICES=0 python train_moe.py --config config/bert_moe_base_all.yaml",
     "NCCL_P2P_DISABLE=1 NCCL_IB_DISABLE=1 CUDA_VISIBLE_DEVICES=0 python train_moe.py --config config/bert_moe_base_11.yaml",
     "NCCL_P2P_DISABLE=1 NCCL_IB_DISABLE=1 CUDA_VISIBLE_DEVICES=0 python train_moe.py --config config/bert_moe_ese_11.yaml",
-    ]
+]
 
 for cmd in cmd_list:
     print(f"\nRunning cmd: {cmd}\n")
     try:
-        # subprocess.run(cmd, shell=True, check=True, stdout=sys.stdout, stderr=sys.stderr)
+        result = subprocess.run(cmd, shell=True, check=True, stdout=sys.stdout, stderr=sys.stderr)
+        print(result.stdout)
         print(f"Command Done: {cmd}")
         print("=" * 100)
     except subprocess.CalledProcessError as e:
-        print(f"Command execution failure: {e}")
-        print("Error message:")
-        print(e.stderr)
+        print(f"Command execution failure: {cmd}")
+        with open("error_log.txt", "a", encoding="utf-8") as log_file:
+            log_file.write(f"FAILED CMD: {cmd}\n")
+            log_file.write(f"RETURN CODE: {e.returncode}\n")
+            log_file.write(f"STDOUT:\n{e.stdout}\n")
+            log_file.write(f"STDERR:\n{e.stderr}\n")
+            log_file.write("=" * 100 + "\n")
