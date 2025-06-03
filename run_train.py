394077947cc15adb5c7b71fc51b227358c1e75ec
DannyWang1922 <1922174653@qq.com>
2025-06-03 10:01:15 +0800

    fix config bug

--- run_train.py
+++ run_train.py
@@ -6,8 +6,8 @@ cmd_list = [
     "NCCL_P2P_DISABLE=1 NCCL_IB_DISABLE=1 CUDA_VISIBLE_DEVICES=0 python train_moe.py --config config/bge_ese.yaml",
     "NCCL_P2P_DISABLE=1 NCCL_IB_DISABLE=1 CUDA_VISIBLE_DEVICES=0 python train_moe.py --config config/bge_moe_base_all.yaml",
     "NCCL_P2P_DISABLE=1 NCCL_IB_DISABLE=1 CUDA_VISIBLE_DEVICES=0 python train_moe.py --config config/bge_moe_ese_all.yaml",
-    "NCCL_P2P_DISABLE=1 NCCL_IB_DISABLE=1 CUDA_VISIBLE_DEVICES=0 python train_moe.py --config config/bge_moe_base_11.yaml"
-    "NCCL_P2P_DISABLE=1 NCCL_IB_DISABLE=1 CUDA_VISIBLE_DEVICES=0 python train_moe.py --config config/bge_moe_moe_11.yaml"
+    "NCCL_P2P_DISABLE=1 NCCL_IB_DISABLE=1 CUDA_VISIBLE_DEVICES=0 python train_moe.py --config config/bge_moe_base_11.yaml",
+    "NCCL_P2P_DISABLE=1 NCCL_IB_DISABLE=1 CUDA_VISIBLE_DEVICES=0 python train_moe.py --config config/bge_moe_ese_11.yaml"
 ]
 
 for cmd in cmd_list:
@@ -21,7 +21,4 @@ for cmd in cmd_list:
         print(f"Command execution failure: {cmd}")
         with open("error_log.txt", "a", encoding="utf-8") as log_file:
             log_file.write(f"FAILED CMD: {cmd}\n")
-            log_file.write(f"RETURN CODE: {e.returncode}\n")
-            log_file.write(f"STDOUT:\n{e.stdout}\n")
-            log_file.write(f"STDERR:\n{e.stderr}\n")
             log_file.write("=" * 100 + "\n")
