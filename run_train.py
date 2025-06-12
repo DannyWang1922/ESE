64fce478a9e630a9d1c880e493ef638ce078fa0e
DannyWang1922 <1922174653@qq.com>
2025-06-12 11:16:50 +0800

    remove bert config; add train_samples exp

--- run_train.py
+++ run_train.py
@@ -12,18 +12,25 @@ import sys
 
 config = "bge_moe_ese_all.yaml"
 epoch = 5
-learning_rate_list = [1e-4, 5e-5, 1e-5]
-num_experts = [16, 8, 4]
-moe_expert_intermediate_size_list = [512, 256]
+# learning_rate_list = [1e-4, 5e-5, 1e-5]
+# num_experts = [16, 8, 4]
+# moe_expert_intermediate_size_list = [512, 256]
+max_train_samples_list = [100000, 300000]
 nv_cmd = "NCCL_P2P_DISABLE=1 NCCL_IB_DISABLE=1 CUDA_VISIBLE_DEVICES=0"
 
+# cmd_list = []
+# for lr in learning_rate_list:
+#     for num_expert in num_experts:
+#         for moe_expert_intermediate_size in moe_expert_intermediate_size_list:
+#             save_dir = f"output/bge_moe_ese_all_{lr}_{num_expert}_{moe_expert_intermediate_size}"
+#             cmd = f"{nv_cmd} python train_moe.py --learning_rate {lr} --num_experts {num_expert} --moe_expert_intermediate_size {moe_expert_intermediate_size} --save_dir {save_dir} --epoch {epoch} --config config/{config}"
+#             cmd_list.append(cmd)
+
 cmd_list = []
-for lr in learning_rate_list:
-    for num_expert in num_experts:
-        for moe_expert_intermediate_size in moe_expert_intermediate_size_list:
-            save_dir = f"output/bge_moe_ese_all_{lr}_{num_expert}_{moe_expert_intermediate_size}"
-            cmd = f"{nv_cmd} python train_moe.py --learning_rate {lr} --num_experts {num_expert} --moe_expert_intermediate_size {moe_expert_intermediate_size} --save_dir {save_dir} --epoch {epoch} --config config/{config}"
-            cmd_list.append(cmd)
+for sample_size in max_train_samples_list:
+    save_dir = f"output/bge_moe_ese_all_{sample_size}"
+    cmd = f"{nv_cmd} python train_moe.py --save_dir {save_dir} --epoch {epoch} --config config/{config} --max_train_samples {sample_size}"
+    cmd_list.append(cmd)
 
 for cmd in cmd_list:
     print(f"\nRunning cmd: {cmd}\n")
