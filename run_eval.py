b1f00773f272a66e8d7953cf495128dabd030ab7
DannyWang1922 <1922174653@qq.com>
2025-06-13 17:21:53 +0800

    update training and eval script

--- run_eval.py
+++ run_eval.py
@@ -1,49 +1,69 @@
 import subprocess
 import sys
 
-base_model  = "models/bert_base" # models/beg_defualt_para, models/uae_github_para, models/qwen_default_para
-model_name = base_model.split("/")[-1]
-ese_model = base_model+"_ese"
+# NCCL_P2P_DISABLE=1 NCCL_IB_DISABLE=1 CUDA_VISIBLE_DEVICES=1 python eval_nli_main.py --is_llm 0 --pooling_strategy cls  --batch_size 512 --model_name_or_path output/bge_base_30w/best-checkpoint --out_dir evl_res/bge_base_30w --is_moe 0
+
+
+model_name_or_path = ["bge_ese"]
+lr_rate_list = ["1e-04", "5e-05", "5e-06", "1e-06"]
 
 nv_cmd = "NCCL_P2P_DISABLE=1 NCCL_IB_DISABLE=1 CUDA_VISIBLE_DEVICES=0"
+batch_size = 512
 
-if "bge" in model_name.lower():
-    baseline_model = "BAAI/bge-base-en-v1.5"
-elif "uae" in model_name.lower():
-    baseline_model = "WhereIsAI/UAE-Large-V1"
-elif "qwen" in model_name.lower():
-    baseline_model = "Qwen/Qwen1.5-0.5B"
-elif "bert" in model_name.lower():
-    baseline_model = "google-bert/bert-base-uncased"
-else:
-    raise ValueError(f"Unknown model name: {model_name}")
+for model_name in model_name_or_path:
+    for lr in  lr_rate_list:
+        model_name_or_path = "train_result/" + model_name + "_" + lr + "/best-checkpoint"
+        out_dir = "evl_res/" + model_name + "_" + lr
+        plot_out_dir = "evl_res/"+ model_name+"/plot"
 
-ese_sts_model_list = ese_model+","+base_model+","+baseline_model
+        if "qwen" not in model_name.lower():
+            is_llm = "0"
+            pooling_strategy = "cls"
+        else:
+            is_llm = "1"
+            pooling_strategy = "mean"
 
-main_out_dir = "evl_res/"+ model_name
-plot_out_dir = "evl_res/"+ model_name+"/plot"
+        if "moe" in model_name.lower():
+            is_moe = "1"
+        else:
+            is_moe = "0"
+        
+        cmd = nv_cmd + f" python eval_nli_main.py --is_llm {is_llm} --pooling_strategy {pooling_strategy}  --batch_size {batch_size } --model_name_or_path {model_name_or_path} --out_dir {out_dir} --is_moe {is_moe}"
+        print(cmd+ "\n")
+    
 
-if "qwen" not in base_model.lower():
-    is_llm = "0"
-    pooling_strategy = "cls"
-else:
-    is_llm = "1"
-    pooling_strategy = "mean"
 
-cmd_list = [
-        f"python eval_nli_main.py --is_llm {is_llm} --pooling_strategy {pooling_strategy}  --model_name_or_path {base_model} --out_dir {main_out_dir}",
-        f"python eval_nli_main.py --is_llm {is_llm} --pooling_strategy {pooling_strategy}  --model_name_or_path {ese_model} --out_dir {main_out_dir}",
-        f"python eval_ese_layers.py --is_llm {is_llm} --pooling_strategy {pooling_strategy}  --model_name_or_path_list {ese_sts_model_list} --out_dir {plot_out_dir}"
-    ]
 
-for cmd in cmd_list:
-    cmd = nv_cmd + " " + cmd
-    print(f"\nRunning cmd: {cmd}\n")
-    try:
-        # subprocess.run(cmd, shell=True, check=True, stdout=sys.stdout, stderr=sys.stderr)
-        print(f"Command Done: {cmd}")
-        print("=" * 100)
-    except subprocess.CalledProcessError as e:
-        print(f"Command execution failure: {e}")
-        print("Error message:")
-        print(e.stderr)
+
+# ese_sts_model_list = ese_model+","+base_model+","+baseline_model
+# main_out_dir = "evl_res/"+ model_name
+
+# if "bge" in model_name.lower():
+#     baseline_model = "BAAI/bge-base-en-v1.5"
+# elif "uae" in model_name.lower():
+#     baseline_model = "WhereIsAI/UAE-Large-V1"
+# elif "qwen" in model_name.lower():
+#     baseline_model = "Qwen/Qwen1.5-0.5B"
+# elif "bert" in model_name.lower():
+#     baseline_model = "google-bert/bert-base-uncased"
+# else:
+#     raise ValueError(f"Unknown model name: {model_name}")
+
+
+# cmd_list = [
+#         f"python eval_nli_main.py --is_llm {is_llm} --pooling_strategy {pooling_strategy}  --model_name_or_path {base_model} --out_dir {main_out_dir}",
+#         f"python eval_nli_main.py --is_llm {is_llm} --pooling_strategy {pooling_strategy}  --model_name_or_path {ese_model} --out_dir {main_out_dir}",
+#         f"python eval_ese_layers.py --is_llm {is_llm} --pooling_strategy {pooling_strategy}  --model_name_or_path_list {ese_sts_model_list} --out_dir {plot_out_dir}"
+#     ]
+
+# for cmd in cmd_list:
+#     cmd = nv_cmd + " " + cmd
+#     print(f"\nRunning cmd: {cmd}\n")
+#     try:
+#         # subprocess.run(cmd, shell=True, check=True, stdout=sys.stdout, stderr=sys.stderr)
+#         print(f"Command Done: {cmd}")
+#         print("=" * 100)
+#     except subprocess.CalledProcessError as e:
+#         print(f"Command execution failure: {e}")
+#         print("Error message:")
+#         print(e.stderr)
--- run_train.py
+++ run_train.py
@@ -10,35 +10,33 @@ import sys
 #     "NCCL_P2P_DISABLE=1 NCCL_IB_DISABLE=1 CUDA_VISIBLE_DEVICES=0 python train_moe.py --config config/bge_moe_ese_11.yaml"
 # ]
 
-config = "bge_moe_ese_all.yaml"
-learning_rate_list = [1e-4, 5e-5, 5e-6, 1e-6]
+# config = "bge_moe_ese_all.yaml"
+config_list = ["bge_ese.yaml"]
+learning_rate_list = [1e-4, 5e-5]
 # num_experts = [16, 8, 4]
 # moe_expert_intermediate_size_list = [512, 256]
-nv_cmd = "NCCL_P2P_DISABLE=1 NCCL_IB_DISABLE=1 CUDA_VISIBLE_DEVICES=0"
-
-# cmd_list = []
-# for lr in learning_rate_list:
-#     for num_expert in num_experts:
-#         for moe_expert_intermediate_size in moe_expert_intermediate_size_list:
-#             save_dir = f"output/bge_moe_ese_all_{lr}_{num_expert}_{moe_expert_intermediate_size}"
-#             cmd = f"{nv_cmd} python train_moe.py --learning_rate {lr} --num_experts {num_expert} --moe_expert_intermediate_size {moe_expert_intermediate_size} --save_dir {save_dir} --config config/{config}"
-#             cmd_list.append(cmd)
+nv_cmd = "NCCL_P2P_DISABLE=1 NCCL_IB_DISABLE=1 CUDA_VISIBLE_DEVICES=1"
 
 cmd_list = []
-for lr in learning_rate_list:
-        save_dir = f"output/bge_moe_ese_all_{lr}"
+for config in config_list:
+    for lr in learning_rate_list:
+        if config == "bge_base.yaml":
+            save_dir = f"train_result/bge_base_{lr}"
+        elif config == "bge_ese.yaml":
+            save_dir = f"train_result/bge_ese_{lr}"
         cmd = f"{nv_cmd} python train_moe.py --learning_rate {lr} --save_dir {save_dir} --config config/{config}"
-        cmd_list.append(cmd)
+        print(cmd)
+#         cmd_list.append(cmd)
 
-for cmd in cmd_list:
-    print(f"\nRunning cmd: {cmd}\n")
-    try:
-        result = subprocess.run(cmd, shell=True, check=True, stdout=sys.stdout, stderr=sys.stderr)
-        print(result.stdout)
-        print(f"Command Done: {cmd}")
-        print("=" * 100)
-    except subprocess.CalledProcessError as e:
-        print(f"Command execution failure: {cmd}")
-        with open("error_log.txt", "a", encoding="utf-8") as log_file:
-            log_file.write(f"FAILED CMD: {cmd}\n")
-            log_file.write("=" * 100 + "\n")
+# for cmd in cmd_list:
+#     print(f"\nRunning cmd: {cmd}\n")
+#     try:
+#         result = subprocess.run(cmd, shell=True, check=True, stdout=sys.stdout, stderr=sys.stderr)
+#         print(result.stdout)
+#         print(f"Command Done: {cmd}")
+#         print("=" * 100)
+#     except subprocess.CalledProcessError as e:
+#         print(f"Command execution failure: {cmd}")
+#         with open("error_log.txt", "a", encoding="utf-8") as log_file:
+#             log_file.write(f"FAILED CMD: {cmd}\n")
+#             log_file.write("=" * 100 + "\n")
