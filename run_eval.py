d3da398fcb696329e1a7cda89446e20df89f654c
DannyWang1922 <1922174653@qq.com>
2025-06-16 14:43:52 +0800

    update train and eval script

--- run_eval.py
+++ run_eval.py
@@ -1,71 +1,58 @@
 import subprocess
 import sys
+from turtle import st
 
 # NCCL_P2P_DISABLE=1 NCCL_IB_DISABLE=1 CUDA_VISIBLE_DEVICES=1 python eval_nli_main.py --is_llm 0 --pooling_strategy cls  --batch_size 512 --model_name_or_path output/bge_base_30w/best-checkpoint --out_dir evl_res/bge_base_30w --is_moe 0
 
 
 # model_name_or_path = ["bge_ese"]
 model_name_or_path = ["bge_moe_ese_all"]
-# lr_rate_list = ["1e-04", "5e-05", "5e-06", "1e-06"]
-wd_list = [2, 4, 6, 8]
+
+
+moe_expert_intermediate_size_list = [256, 512]
+ese_compression_size_list =[32, 64, 128, 256, 512, 640, 768]
+
 nv_cmd = "NCCL_P2P_DISABLE=1 NCCL_IB_DISABLE=1 CUDA_VISIBLE_DEVICES=1"
 batch_size = 512
+embedding_size_list = "32,64,128,256,512,640,768"
+cmd_list = []
 
 for model_name in model_name_or_path:
-    for lr in  wd_list:
-        lr = str(lr)
-        model_name_or_path = "train_result/" + model_name + "_" + lr + "/best-checkpoint"
-        out_dir = "evl_res/" + model_name + "_" + lr
-        plot_out_dir = "evl_res/"+ model_name+"/plot"
+    for moe_expert_intermediate_size in moe_expert_intermediate_size_list:
+        for ese_compression_size in ese_compression_size_list:
+            moe_expert_intermediate_size = str(moe_expert_intermediate_size)
+            ese_compression_size = str(ese_compression_size)
+            model_name_or_path = f"train_result/{model_name}_{moe_expert_intermediate_size}_{ese_compression_size}/best-checkpoint"
+            out_dir = f"evl_res/{model_name}_{moe_expert_intermediate_size}_{ese_compression_size}/plot"
+          
+            plot_out_dir = "evl_res/"+ model_name+"/plot"
 
-        if "qwen" not in model_name.lower():
-            is_llm = "0"
-            pooling_strategy = "cls"
-        else:
-            is_llm = "1"
-            pooling_strategy = "mean"
+            if "qwen" not in model_name.lower():
+                is_llm = "0"
+                pooling_strategy = "cls"
+            else:
+                is_llm = "1"
+                pooling_strategy = "mean"
 
-        if "moe" in model_name.lower():
-            is_moe = "1"
-        else:
-            is_moe = "0"
-        
-        cmd = nv_cmd + f" python eval_nli_main.py --is_llm {is_llm} --pooling_strategy {pooling_strategy}  --batch_size {batch_size } --model_name_or_path {model_name_or_path} --out_dir {out_dir} --is_moe {is_moe}"
-        print(cmd+ "\n")
-    
-
-
-
-
-# ese_sts_model_list = ese_model+","+base_model+","+baseline_model
-# main_out_dir = "evl_res/"+ model_name
-
-# if "bge" in model_name.lower():
-#     baseline_model = "BAAI/bge-base-en-v1.5"
-# elif "uae" in model_name.lower():
-#     baseline_model = "WhereIsAI/UAE-Large-V1"
-# elif "qwen" in model_name.lower():
-#     baseline_model = "Qwen/Qwen1.5-0.5B"
-# elif "bert" in model_name.lower():
-#     baseline_model = "google-bert/bert-base-uncased"
-# else:
-#     raise ValueError(f"Unknown model name: {model_name}")
+            if "moe" in model_name.lower():
+                is_moe = "1"
+            else:
+                is_moe = "0"
+            
+            # cmd = nv_cmd + f" python eval_nli_main.py --is_llm {is_llm} --pooling_strategy {pooling_strategy}  --batch_size {batch_size} --model_name_or_path {model_name_or_path} --out_dir {out_dir} --is_moe {is_moe}"
+            cmd = nv_cmd + f" python eval_ese_layers.py --is_llm {is_llm} --pooling_strategy {pooling_strategy}  --batch_size {batch_size} --model_name_or_path {model_name_or_path} --out_dir {out_dir} --is_moe {is_moe} --embedding_size_list {embedding_size_list}"
+            # print(cmd+ "\n")
+            cmd_list.append(cmd)
+            
 
 
-# cmd_list = [
-#         f"python eval_nli_main.py --is_llm {is_llm} --pooling_strategy {pooling_strategy}  --model_name_or_path {base_model} --out_dir {main_out_dir}",
-#         f"python eval_nli_main.py --is_llm {is_llm} --pooling_strategy {pooling_strategy}  --model_name_or_path {ese_model} --out_dir {main_out_dir}",
-#         f"python eval_ese_layers.py --is_llm {is_llm} --pooling_strategy {pooling_strategy}  --model_name_or_path_list {ese_sts_model_list} --out_dir {plot_out_dir}"
-#     ]
-
-# for cmd in cmd_list:
-#     cmd = nv_cmd + " " + cmd
-#     print(f"\nRunning cmd: {cmd}\n")
-#     try:
-#         # subprocess.run(cmd, shell=True, check=True, stdout=sys.stdout, stderr=sys.stderr)
-#         print(f"Command Done: {cmd}")
-#         print("=" * 100)
-#     except subprocess.CalledProcessError as e:
-#         print(f"Command execution failure: {e}")
-#         print("Error message:")
-#         print(e.stderr)
+for cmd in cmd_list:
+    print(f"\nRunning cmd: {cmd}\n")
+    try:
+        # subprocess.run(cmd, shell=True, check=True, stdout=sys.stdout, stderr=sys.stderr)
+        print(f"Command Done: {cmd}")
+        print("=" * 100)
+    except subprocess.CalledProcessError as e:
+        print(f"Command execution failure: {e}")
+        print("Error message:")
+        print(e.stderr)
--- run_train.py
+++ run_train.py
@@ -10,27 +10,24 @@ import sys
 #     "NCCL_P2P_DISABLE=1 NCCL_IB_DISABLE=1 CUDA_VISIBLE_DEVICES=0 python train_moe.py --config config/bge_moe_ese_11.yaml"
 # ]
 
-# config = "bge_moe_ese_all.yaml"
-loss_weight_decay_list = [0.0001, 0.005, 0.001, 0.05, 0.1, 500, 1000]
 config_list = ["bge_moe_ese_all.yaml"]
-learning_rate_list = [5e-6]
-lr = "5e-6"
-# num_experts = [16, 8, 4]
-# moe_expert_intermediate_size_list = [512, 256]
+ese_compression_size_list =[32, 64, 128, 256, 512, 640, 768]
+moe_expert_intermediate_size_list = [256, 512]
 nv_cmd = "NCCL_P2P_DISABLE=1 NCCL_IB_DISABLE=1 CUDA_VISIBLE_DEVICES=0"
 
 cmd_list = []
 for config in config_list:
-    for wd in loss_weight_decay_list:
-        if config == "bge_base.yaml":
-            save_dir = f"train_result/bge_base_{wd}"
-        elif config == "bge_ese.yaml":
-            save_dir = f"train_result/bge_ese_{wd}"
-        elif config == "bge_moe_ese_all.yaml":
-            save_dir = f"train_result/bge_moe_ese_all_{wd}"
-        cmd = f"{nv_cmd} python train_moe.py --learning_rate {lr} --save_dir {save_dir} --config config/{config} --loss_weight_decay {wd}"
-        print(cmd)
-#         cmd_list.append(cmd)
+    for moe_expert_intermediate_size in moe_expert_intermediate_size_list:
+        for ese_compression_size in ese_compression_size_list:
+            if config == "bge_base.yaml":
+                save_dir = f"train_result/bge_base_{moe_expert_intermediate_size}_{ese_compression_size}"
+            elif config == "bge_ese.yaml":
+                save_dir = f"train_result/bge_ese_{moe_expert_intermediate_size}_{ese_compression_size}"
+            elif config == "bge_moe_ese_all.yaml":
+                save_dir = f"train_result/bge_moe_ese_all_{moe_expert_intermediate_size}_{ese_compression_size}"
+            cmd = f"{nv_cmd} python train_moe.py --save_dir {save_dir} --config config/{config} --moe_expert_intermediate_size {moe_expert_intermediate_size} --ese_compression_size {ese_compression_size} --moe_expert_compressed_size {ese_compression_size}"
+            print(cmd)
+            # cmd_list.append(cmd)
 
 # for cmd in cmd_list:
 #     print(f"\nRunning cmd: {cmd}\n")
